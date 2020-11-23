b904203ef1704e5177e85758430909e39851d5a5
thomas <Thomas>
2020-11-23 10:20:11 +0100

    remove unused library

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-g++ -std=c++2a -o3 -m64 -I/usr/include/python3.8 -fopenmp -Wall -Wextra main.cpp lib.cpp -lpython3.8 -o main
+g++ -std=c++2a -o3 -m64 -I/usr/include/python3.8 -fopenmp -Wall -Wextra main.cpp lib.cpp -o main
 
 if [ $? -eq 0 ]; then
     echo ---Builded!---
