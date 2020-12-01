ed9cd57b583e7c30511c3381465fe51c712a635e
thomas <Thomas>
2020-12-01 18:06:24 +0100

    very strange

--- debug.sh
+++
@@ -1,9 +0,0 @@
-g++ -std=c++2a -o3 -m64 -fopenmp -Wall -Wextra -D SIMULATION -D DEBUG main.cpp lib.cpp -o main
-
-if [ $? -eq 0 ]; then
-    echo ---Builded!---
-    ./main
-    echo -----END------
-else
-    echo Build Failed!
-fi 
