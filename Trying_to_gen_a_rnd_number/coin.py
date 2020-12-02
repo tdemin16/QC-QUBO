dde2a0202e87355f88239527b54a36e7534507de
thomas <Thomas>
2020-12-02 12:55:47 +0100

    Well works

--- Trying_to_gen_a_rnd_number/coin.py
+++ Trying_to_gen_a_rnd_number/coin.py
@@ -3,25 +3,22 @@ from dwave.system.samplers import DWaveSampler
 from neal import SimulatedAnnealingSampler
 from dwave.system.composites import EmbeddingComposite
 
-def dec(n):
-    num = n
-    dec_value = 0
 
-    # Initializing base
-    # value to 1, which is 2 ^ 0
+def dec(num):
+    dec_value = 0
     base1 = 1
 
     len1 = len(num)
-    for i in range(len1 -1, -1, -1):
-        if (num[i] == '1'):
+    for i in range(len1 - 1, -1, -1):
+        if (num[i] == 1):
             dec_value += base1
-            base1 = base1 * 2
-            pass
-        pass
+        
+        base1 = base1 * 2
 
     return dec_value
 
-useQpu = False
+
+useQpu = True
 
 if(useQpu):
     sampler = DWaveSampler()
@@ -30,21 +27,23 @@ else:
     sampler = SimulatedAnnealingSampler()
 
 bqm = {}
-distrib = {} #don't think I need
+distrib = {}  # don't think I need
 
 bits = 4
 
 for i in range(0, bits):
-    bqm[(i,i)] = 0
+    bqm[(i, i)] = 0
     pass
 
 start = time.time()
 response = sampler.sample_qubo(bqm, num_reads=1)
 total = time.time() - start
 
-num = ""
+num = []
 for datum in response.data():
     for key in datum.sample:
-        num += str(datum.sample[key])
+        num.append(datum.sample[key])
 
+print(total)
+print(num)
 print(dec(num))
